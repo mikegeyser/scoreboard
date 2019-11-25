8f97b6d61e0369457d97cac5a956d065fb4f936f
Mike Geyser <mikegeyser@gmail.com>
2019-11-25 22:03:37 +0200

    Made all of the tests pass.

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,20 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { initialState, getGameScore, setScore } from "../scoreboard";
 
 class Scoreboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
   render() {
+    const { scoreCall } = getGameScore(this.state.gamePoints);
+
     return (
       <div>
         <h1>Tennis Scoreboard</h1>
-        <h2 id="score">Score: love-all</h2>
-        <button className="player1-scores" type="button">
+        <h2 id="score">Score: {scoreCall}</h2>
+        <button
+          className="player1-scores"
+          type="button"
+          onClick={() => this.handlePlayerScore(1)}
+        >
           Player 1 scores
         </button>
-        <button className="player2-scores" type="button">
+        <button
+          className="player2-scores"
+          type="button"
+          onClick={() => this.handlePlayerScore(2)}
+        >
           Player 2 scores
         </button>
       </div>
     );
-  };
+  }
+
+  handlePlayerScore(player) {
+    const update = setScore(player, this.state);
+    this.setState(update);
+  }
 }
 
 export default Scoreboard;
--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,24 +1,75 @@
 export const initialState = {
   gamePoints: {
     player1: 0,
-    player2: 0,
-  },
+    player2: 0
+  }
 };
 
 export function setScore(playerNumber, previousState) {
+  const player = `player${playerNumber}`;
+
+  let gamePoints = Object.assign({}, previousState.gamePoints, {
+    [player]: previousState.gamePoints[player] + 1
+  });
+
+  // Todo: This is clunky, but any attempt to shorten it made the result far 
+  // less legible, obscuring meaning.
+  const isDoubleDeuce = gamePoints.player1 === 4 && gamePoints.player2 === 4;
+
+  if (isDoubleDeuce) {
+    gamePoints = {
+      player1: 3,
+      player2: 3
+    };
+  }
+
   return {
-    gamePoints: Object.assign({}, previousState.gamePoints, {
-      [`player${playerNumber}`]: previousState.gamePoints[`player${playerNumber}`] + 1
-    })
+    gamePoints
   };
 }
 
 export function getGameScore(gamePoints) {
+  const { player1, player2 } = gamePoints;
 
-  // ... do something with gamePoints ...
+  const pointsAreEqual = player1 === player2;
+  const isDeuce = pointsAreEqual && player1 === 3;
+
+  if (isDeuce) {
+    return { scoreCall: "Deuce" };
+  }
+
+  if (pointsAreEqual) {
+    return { scoreCall: `${getCall(player1)}-all` };
+  }
+
+  const leadingPlayer = player1 > player2 ? "player1" : "player2";
+
+  if (gamePoints[leadingPlayer] > 3) {
+    return {
+      scoreCall: `Game, ${leadingPlayer}`,
+      winningPlayer: leadingPlayer
+    };
+  }
 
   return {
-    scoreCall: null,
-    winningPlayer: null,
+    scoreCall: `${getCall(player1)}-${getCall(player2)}`,
+    winningPlayer: null
+  };
+}
+
+function getCall(points) {
+  switch (points) {
+    case 0:
+      return "love";
+    case 1:
+      return "15";
+    case 2:
+      return "30";
+    case 3:
+      return "40";
+    case 4:
+      return "Advantage";
+    default:
+      throw Error("Invalid score.");
   }
 }
